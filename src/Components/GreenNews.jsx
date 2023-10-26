b04307f2e64625e16c96353743cdb2dfb4b8e6fd
SAIDI <mahrez.saidi@epsi.fr>
2023-10-26 09:46:13 +0200

    firstPage

---
+++ src/Components/GreenNews.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function GreenNews() {
+  return (
+   <></>
+  );
+}
+
+export default GreenNews;
---
+++ src/Components/MaConsommation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MaConsommation() {
+  return (
+    <div className='conso'>
+      <h1>Option 2</h1>
+      <p>Ceci est le contenu de l'Option 2.</p>
+    </div>
+  );
+}
+
+export default MaConsommation;
